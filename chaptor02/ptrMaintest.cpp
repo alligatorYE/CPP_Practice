19f4e2da059862121248844444efb05bafaf89b9
alligatorYE <alligator_@outlook.com>
2021-04-09 13:55:32 +0800

    add chapt

---
+++ chaptor02/ptrMaintest.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int main(int argc, const char **argv)
+{
+    int ivalue = 10;
+    std::cout << "ivalue = " << ivalue << std::endl;
+
+    int &refi = ivalue;
+    std::cout << "refi = " << refi << std::endl;
+
+    int *ptr = &refi;
+    std::cout << "*ptr = " << *ptr << "\t"
+              << "ptr = " << ptr << "\t"
+              << "&ptr = " << &ptr << std::endl;
+
+    int *ptr2 = ptr;
+    std::cout << "*ptr2 = " << *ptr2 << "\t"
+              << "ptr2 = " << ptr2 << "\t"
+              << "&ptr2 = " << &ptr2 << std::endl;
+
+    refi = 23;
+
+    std::cout << "ivalue = " << ivalue << std::endl;
+    std::cout << "*ptr = " << *ptr << "\t"
+              << "ptr = " << ptr << "\t"
+              << "&ptr = " << &ptr << std::endl;
+    std::cout << "*ptr2 = " << *ptr2 << "\t"
+              << "ptr2 = " << ptr2 << "\t"
+              << "&ptr2 = " << &ptr2 << std::endl;
+
+    int *p1 = nullptr;
+
+    int *p2 = 0;
+
+    int *p3 = NULL;
+    return 0;
+}
