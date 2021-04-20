6b4f75e8bee31ebf0196bec10bca9f07e1ab5c27
alligator <alligator_@outlook.com>
2021-04-20 22:58:21 +0800

    update auto type notes

--- section2/ptrMaintest.cpp
+++ section2/ptrMaintest.cpp
@@ -90,16 +90,18 @@ int main(int argc, const char **argv)
     const double *const ppi = &pi; //ppi是一个指向常量的指针
     std::cout << "*ppi = " << *ppi << std::endl;
 
-    int ivalue2 = 0;
-    int *const ptr3 = &ivalue2; //ptr3是一个顶层const，只能指向&ivalue2，不能指向其他地址，自身不能被修改。
+    int ivalue3 = 0;
+    int *const ptr3 = &ivalue3; //ptr3是一个顶层const，只能指向&ivalue3，不能指向其他地址，自身不能被修改。[自己不动，让别人动]
     std::cout << "*ptr3 = " << *ptr3 << std::endl;
-    ivalue2 = -1;
+    ivalue3 = -1;
+    std::cout << "*ptr3 = " << *ptr3 << std::endl;
+    *ptr3 = 100;
     std::cout << "*ptr3 = " << *ptr3 << std::endl;
 
-    const int civalue = 44;
-    const int *ptr4 = &civalue; //ptr4是一个底层const，只读&ivalue，不能对ivalue的值进行改变，自身可以被修改。
+    const int ivalue4 = 44;
+    const int *ptr4 = &ivalue4; //ptr4是一个底层const，只读&ivalue，不能对ivalue的值进行改变，自身可以被修改。[不能动别人，只能动自己]
     std::cout << "*ptr4 = " << *ptr4 << std::endl;
-    ptr4 = &ivalue2;
+    ptr4 = &ivalue3;
     std::cout << "*ptr4 = " << *ptr4 << std::endl;
 
     constexpr int *ptr5 = nullptr; //ptr5 是一个指向整型的 常量指针
@@ -116,7 +118,47 @@ int main(int argc, const char **argv)
     };
     using SI = Sales_item; //C++11 新标准，给对象取别名
 
-    
+    //auto 类型说明符
+    double db;
+    float fl;
+    auto item = db + fl;
+
+    std::cout << "size of item: " << sizeof(item) << std::endl;
+
+    //auto一般会忽略掉const，同时底层const则会保存下来。
+    int i = 0;
+
+    const int ci1 = i, &cr = ci1;
+    auto b = ci1;   //b是一个整数(ci1的顶层特性被忽略了)
+    b = 2; //b可以被修改
+    auto c = cr;    //c是一个整数(cr是ci1的别名，ci本身是一个顶层const)
+
+
+    c = 3;//c可以被修改
+    auto d = &i;    //d是一个整型指针(整数的地址就是指向整数的指针)
+    auto e = &ci1;
+    std::cout << "b = " << b <<"\t"
+              << "c = " << c <<"\t"
+              << "d = " << d <<"\t"
+              << "e = " << e <<std::endl;
+    //如果需要auto推断出顶层const，需要明确指出
+
+    const auto f  = ci;
+    //f = 6; 会提示：“表达式必须是可修改的左值”
+
+    auto &g = ci;
+    // auto &h = 42; 会报错，不能为非常量引用绑定字面值
+    const auto &j = 42; //可以为常量引用绑定字面值
+    //使用auto在一条语句中定义多个变量时，“&” 和“*”只从属于某一个声明符，而非基本数据类型的一部分，因此初始值必须是同一种类型
+    auto k = ci1, &l = i;//k是整数，l是整型引用
+    auto &m = ci1, *pa = &ci1; //m是对整型常量的引用，p是指向整型常量的指针 
+    //auto &n = i, *pa2 = &ci1; 错误：i的类型是int而&ci1的类型是const int，初始值不是同一种类型
+
+    //***************************** decltype ***********************************
 
     return 0;
+}
+
+namespace auto_type_description {
+
 }
