79159cfbfec7745ac59aede78dc44eda5780f1c4
Y.E <alligator_@outlook.com>
2021-11-21 18:43:28 +0800

    使用sizeof 确定变量的长度

--- Src/main.cpp
+++ Src/main.cpp
@@ -2,9 +2,9 @@
 // Created by alligator on 2021/11/21.
 //
 /*
- * 演示溢出错误
- * Demo overflow error
- * 程序清单3.4 有符号整型变量和无符号整型变量溢出的负面影响
+ * 使用sizeof 确定变量的长度
+ * Use sizeof to determine the length of the variable
+ * 程序清单3.5 获悉标准C++变量类型的长度
  * */
 
 #include <iostream>
@@ -15,13 +15,21 @@ using std::endl;
 
 int main(int argc, char const *argv[])
 {
+    cout << "Computing the size of some C++ inbuilt variable types" << endl;
+    cout << "Size of boolean variables: " << sizeof(bool) << endl;
+    cout << "size of char variables: " << sizeof(char) << endl;
+    cout << "size of unsigned short int variables: " << sizeof(unsigned short int) << endl;
+    cout << "size of short int variables: " << sizeof(short int) << endl;
+    cout << "size of unsigned long int variables: " << sizeof(unsigned long int) << endl;
+    cout << "size of long variables: " << sizeof(long) << endl;
+    cout << "size of int variables: " << sizeof(int) << endl;
+    cout << "size of unsigned long long variables: " << sizeof(unsigned long long) << endl;
+    cout << "size of long long variables: " << sizeof(long long) << endl;
+    cout << "size of unsigned int variables: " << sizeof(unsigned int) << endl;
+    cout << "size of float variables: " << sizeof(float) << endl;
+    cout << "size of double variables: " << sizeof(double) << endl;
 
-    unsigned short uShortValue = 65535; //unsigned short, 它占用16 位内存，因此取值范围为0～65535
-    cout << "Incrementing unsigned short " << uShortValue << "gives: " ;
-    cout << ++uShortValue <<"\n" << endl;
+    cout << "The output changes with compiler, hardware and OS" << endl;
 
-    short signedShort = 32767; //signed short 的取值范围为−32768～32768
-    cout << "Incrementing signed short " << signedShort << "gives: " ;
-    cout << ++signedShort <<"\n" << endl;
     return 0;
 }
