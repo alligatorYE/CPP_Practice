9e7883ab6a0df72fb6856178487147d40c9a4a6f
Y.E <alligator_@outlook.com>
2021-11-21 20:02:30 +0800

    使用auto 自动推断类型

--- Src/main.cpp
+++ Src/main.cpp
@@ -2,7 +2,8 @@
 // Created by alligator on 2021/11/21.
 //
 /*
- * 使用列表初始化避免缩窄转换错误
+ * 使用auto 自动推断类型
+ * 程序清单3.6 使用关键字auto 依靠编译器的类型推断功能
  * */
 
 #include <iostream>
@@ -13,12 +14,12 @@ using std::endl;
 
 int main(int argc, char const *argv[])
 {
+    auto coinFlippedHeads = true;
+    auto longNumber = 2500000000000;
+    cout << "coinFlippedHeads = " << coinFlippedHeads << endl;
+    cout << "size of coinFlippedHeads: " << sizeof(coinFlippedHeads) << endl;
 
-    int largeNum = 5000000;
-//    short smallNum = largeNum; // compiles OK, yet narrowing error
-//    short anotherNum{ largeNum }; // error! Amend types
-    int anotherNum{ largeNum }; // OK!
-//    float someFloat{ largeNum }; // error! An int may be narrowed
-    float someFloat{ 5000000 }; // OK! 5000000 can be accomodated
+    cout << "longNumber = " << longNumber << endl;
+    cout << "size of longNumber: " << sizeof(longNumber) << endl;
     return 0;
 }
