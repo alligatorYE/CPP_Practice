e189027efde084a7976f2ce86e2eaf4eb79fc44e
Y.E <alligator_@outlook.com>
2021-11-22 19:37:41 +0800

    create base

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,10 +1,7 @@
 //
 // Created by alligator on 2021/11/21.
 //
-/*
- * 使用constexpr 定义常量表达式
- * 程序清单3.8 使用常量表达式来计算pi 的值
- * */
+
 
 #include <iostream>
 #include <string>
@@ -12,16 +9,7 @@ using std::string;
 using std::cout;
 using std::endl;
 
-constexpr double GetPi() { return 22.0 / 7; }
-constexpr double TwicePi() { return 2 * GetPi(); }
-
 int main(int argc, char const *argv[])
 {
-    using namespace std;
-    const double pi = 22.0 / 7;
-
-    cout << "constant pi contains value " << pi << endl;
-    cout << "constexpr GetPi() returns value " << GetPi() << endl;
-    cout << "constexpr TwicePi() returns value " << TwicePi() << endl;
     return 0;
 }
