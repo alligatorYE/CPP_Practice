bc256b2081bc258c50a79cd793ccd0f1578d928c
alligator <alligator_@outlook.com>
2021-11-23 23:09:51 +0800

    add gitee remote repository

--- Src/main.cpp
+++ Src/main.cpp
@@ -2,6 +2,12 @@
 // Created by alligator on 2021/11/21.
 //
 
+/* git notes
+ * ssh-keygen -t rsa -C "Alligator_@outlook.com"
+ * clip < ~/.ssh/github_id_rsa.pub
+ * 为git仓库配置远程仓库
+ * git remote add gitee https://gitee.com/alligatorYE/CPP_Practice.git
+ * */
 
 #include <iostream>
 #include <string>
