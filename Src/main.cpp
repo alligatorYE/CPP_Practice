dfcfc7234bd1e8bb1207e51527709210dfc166aa
alligator <alligator_@outlook.com>
2022-03-05 15:58:46 +0800

    add function reload

--- Src/main.cpp
+++ Src/main.cpp
@@ -2,7 +2,7 @@
 // Created by alligator on 2021/11/21.
 //
 
-/* git notes
+/** git notes
  * ssh-keygen -t rsa -C "Alligator_@outlook.com"
  * clip < ~/.ssh/github_id_rsa.pub
  * 为git仓库配置远程仓库
@@ -11,11 +11,44 @@
 
 #include <iostream>
 #include <string>
+
 using std::string;
 using std::cout;
+using std::cin;
 using std::endl;
 
-int main(int argc, char const *argv[])
-{
+const double Pi = 3.14159265;
+
+double Area(double radius); //for circle
+double Area(double radius, double height); // for cylinder
+
+int main(int argc, char const *argv[]) {
+    cout << "Enter z for Cylinder, c for Circle: ";
+    char userSelection = 'z';
+    cin >> userSelection;
+
+    cout << "Enter radius: ";
+    double radius = 0;
+    cin >> radius;
+
+    if (userSelection == 'z') {
+        cout << "Enter height: ";
+        double height = 0;
+        cin >> height;
+
+        // Invoke overloaded variant of Area for Cyclinder
+        cout << "Area of cylinder is: " << Area(radius, height) << endl;
+    } else {
+        cout << "Area of cylinder is: " << Area(radius) << endl;
+    }
     return 0;
 }
+
+double Area(double radius) {
+    return Pi * radius * radius;
+}
+
+double Area(double radius, double height) {
+    return Area(radius) * 2 + 2 * Pi * radius * height;
+}
+
