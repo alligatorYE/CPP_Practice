b941ad3b27b806478083b0dd9c66a72bbdb9b76a
alligator <alligator_@outlook.com>
2022-03-05 18:10:18 +0800

    add ParameterByArray

--- Src/main.cpp
+++ Src/main.cpp
@@ -11,11 +11,44 @@
 
 #include <iostream>
 #include <string>
+#include <algorithm>
 using std::string;
 using std::cout;
 using std::endl;
+using std::sort;
+
+void DisplayArray(int numbers[], int length)
+{
+    for (int index = 0; index < length; ++index)
+    {
+        cout << numbers[index] << " ";
+    }
+
+    cout << endl;
+
+//    std::sort(numbers, numbers + 4);
+}
+
+
+void DisplayArray(char characters[], int length)
+{
+    for (int index = 0; index < length; ++index)
+    {
+        cout << characters[index] << " ";
+    }
+    cout << endl;
+}
 
 int main(int argc, char const *argv[])
 {
+    int myNums[4] = {24, 58, -1, 245};
+    DisplayArray(myNums, 4);
+
+    char myStatement[7] = {'H', 'e', 'l', 'l', 'o', '!', '\0'};
+    DisplayArray(myStatement, 7);
+
+    std::sort(myNums, myNums + 4);
+    DisplayArray(myNums, 4);
+
     return 0;
 }
