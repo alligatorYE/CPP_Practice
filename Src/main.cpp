b9dd72d4d499904576f8e4fb6add7aa281706c6f
alligator <alligator_@outlook.com>
2022-03-05 17:09:48 +0800

    parameter by reference

--- Src/main.cpp
+++ Src/main.cpp
@@ -13,9 +13,28 @@
 #include <string>
 using std::string;
 using std::cout;
+using std::cin;
 using std::endl;
+
+const double Pi = 3.1416;
+
+// output parameter result by reference
+/**
+ * 第二个形参result 旁边的&，它告诉编译器，不要将第二个实参复制给函数，而将指向该实参的引用传递给函数
+ * */
+void Area(double radius, double& result)
+{
+    result = Pi * radius * radius;
+}
 
 int main(int argc, char const *argv[])
 {
+    cout << "Enter radius: ";
+    double radius = 0;
+    cin >> radius;
+
+    double areaFetched = 0;
+    Area(radius, areaFetched);
+    cout << "The area is: " << areaFetched << endl;
     return 0;
 }
