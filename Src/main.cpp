1de9017edefe0d9772c3b6554dffd97151666683
alligatorYE <alligator_@outlook.com>
2022-03-10 21:56:23 +0800

    lambda expression

--- Src/main.cpp
+++ Src/main.cpp
@@ -15,11 +15,30 @@
 
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <vector>
+
 using std::string;
 using std::cout;
 using std::endl;
+using std::vector;
+
+void DisplayNums(vector<int>& dynArray)
+{
+    std::for_each(dynArray.begin(),dynArray.end(),[](int Element) {cout << Element <<" ";});
+    cout << std::endl;
+}
 
 int main(int argc, char const *argv[])
 {
+    vector<int> myNums;
+    myNums.push_back(501);
+    myNums.push_back(-1);
+    myNums.push_back(25);
+    myNums.push_back(-35);
+    DisplayNums(myNums);
+    cout << "Sorting them in descending order" << endl;
+    std::sort(myNums.begin(),myNums.end(),[](int num1, int num2) {return num2 < num1;});
+    DisplayNums(myNums);
     return 0;
 }
