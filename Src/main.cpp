38c305f1c8dddee89ddd50f31d317aafc325d08f
alligatorYE <alligator_@outlook.com>
2022-07-14 22:24:25 +0800

    add git notes in main.cpp

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,9 +1,16 @@
 //
 // Created by alligator on 2021/11/21.
 //
-/*
- * 使用auto 自动推断类型
- * 程序清单3.6 使用关键字auto 依靠编译器的类型推断功能
+/** git notes
+ * ssh-keygen -t rsa -C "Alligator_@outlook.com"
+ * clip < ~/.ssh/github_id_rsa.pub
+ * 为git仓库配置远程仓库
+ * git remote add gitee https://gitee.com/alligatorYE/CPP_Practice.git
+ * git remote -v # 查看远程分支
+ * git remote rm gitee # 移除远程分支gitee
+ * 解决[ OpenSSL SSL_read: Connection was reset, errno 10054] 办法：git config --global http.sslVerify "false"
+ * vim esc键无法退出编辑模式，使用F5键退出编辑模式
+ * git log --oneline --graph --all --decorate
  * 将notes 添加到dev分支中: git checkout -p 21_8.7 notes/*.*
  * */
 
